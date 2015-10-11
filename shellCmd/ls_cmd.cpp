bf5f4629f0afc3330912237ab534c89b44cf5484
Rajmani Arya <rajmani1995@gmail.com>
2015-10-11 14:11:01 +0530

    added ls -l, ls -r, ls -a and ls features

--- shellCmd/ls_cmd.cpp
+++ shellCmd/ls_cmd.cpp
@@ -1,73 +1,111 @@
-#include <iostream>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <string.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <dirent.h>
+#include <string.h>
+#include <sys/dir.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include <locale.h>
 
-#define INVALID_ARGS 3
-#define NO_ARGS 1
-
-using namespace std;
-
-void fullOpt(char path[]) {
-
+void print_perms(mode_t st) {
+    char perms[11];
+    if(st && S_ISREG(st)) perms[0]='-';
+    else if(st && S_ISDIR(st)) perms[0]='d';
+    else if(st && S_ISFIFO(st)) perms[0]='|';
+    else if(st && S_ISSOCK(st)) perms[0]='s';
+    else if(st && S_ISCHR(st)) perms[0]='c';
+    else if(st && S_ISBLK(st)) perms[0]='b';
+    else perms[0]='l';  // S_ISLNK
+    perms[1] = (st && S_IRUSR) ? 'r':'-';
+    perms[2] = (st && S_IWUSR) ? 'w':'-';
+    perms[3] = (st && S_IXUSR) ? 'x':'-';
+    perms[4] = (st && S_IRGRP) ? 'r':'-';
+    perms[5] = (st && S_IWGRP) ? 'w':'-';
+    perms[6] = (st && S_IXGRP) ? 'x':'-';
+    perms[7] = (st && S_IROTH) ? 'r':'-';
+    perms[8] = (st && S_IWOTH) ? 'w':'-';
+    perms[9] = (st && S_IXOTH) ? 'x':'-';
+    perms[10] = '\0';
+    printf("%s", perms);
 }
-void showRec(char path[]) {
-	dirent ** files; struct stat statbuf;
-	cout << "\n" << path << ":\n";
-	int n = scandir(path, &files, 0, alphasort);
-	char tmp[128], pth[128];
-	if(n < 0)
-		cout << "Error Ocuured !\n";
-	else {
-		for(int i=n-1; i>=0; i--) {
-			strcpy(pth, path);
-			strcat(pth, "/");
-			strcat(pth, files[i]->d_name);
-			
-			if(files[i]->d_name[0] != '.' && stat(pth, &statbuf) != -1) {
-   				if (!S_ISDIR(statbuf.st_mode))
-   					cout << files[i]->d_name << "  ";
-   				else
-   					showRec(pth);
-   			}
-   		}
-   	}
-	
+
+void ls(char path[], int flag) {
+    DIR * dir; dirent * file;
+    dir = opendir(path);
+    while(file=readdir(dir)) {
+        if(file->d_name[0] != '.' || flag) {
+            printf("%s  ", file->d_name);
+        }
+    }
+    free(file);
+    free(dir);
 }
-void showFiles(bool all, char path[]){
-	dirent ** files;
-	int n = scandir(path, &files, 0,alphasort);
-	if(n < 0)
-		cout << "Error Ocuured !\n";
-	else {
-		for(int i=0; i<n; i++)
-			if(files[i]->d_name[0] != '.' || all)
-				cout << files[i]->d_name << "  ";
-	}
+void ls_l(char path[]) {
+    DIR * dir; dirent * file; struct stat sbuf;
+    char buf[128];
+    struct passwd pwent, * pwentp;
+    struct group grp, * grpt;
+    char datestring[256];
+    struct tm time;
+    dir = opendir(path);
+    while(file=readdir(dir)) {
+        stat(file->d_name, &sbuf);
+        print_perms(sbuf.st_mode);
+        printf(" %d", (int)sbuf.st_nlink);
+        if (!getpwuid_r(sbuf.st_uid, &pwent, buf, sizeof(buf), &pwentp))
+            printf(" %s", pwent.pw_name);
+        else
+            printf(" %d", sbuf.st_uid);
+
+        if (!getgrgid_r (sbuf.st_gid, &grp, buf, sizeof(buf), &grpt))
+            printf(" %s", grp.gr_name);
+        else
+            printf(" %d", sbuf.st_gid);
+        printf(" %5d", (int)sbuf.st_size);
+        
+        localtime_r(&sbuf.st_mtime, &time);
+        /* Get localized date string. */
+        strftime(datestring, sizeof(datestring), "%F %T", &time);
+
+        printf(" %s %s\n", datestring, file->d_name);
+    }
 }
-/* Main function starts here */
+void ls_r(char path[]) {
+    DIR * dir; dirent * file; struct stat sbuf;
+    char tmp[128];
+    dir = opendir(path);
+    while(file=readdir(dir)) {
+        if(file->d_name[0] == '.') continue;
+        strcpy(tmp, path);
+        strcat(tmp, "/");
+        strcat(tmp, file->d_name);
+        //printf("%s\n", tmp);
+        stat(tmp, &sbuf);
+        if(sbuf.st_mode && S_ISDIR(sbuf.st_mode)) {
+            printf("\n%s/\n", tmp);
+            ls_r(tmp);
+        } else 
+            printf("%s  ", file->d_name);
+    }
+}
 int main(int argc, char * argv[]) {
-	
-	char path[128];
-	getcwd(path, 128);
-	
-	if(argc == 2){
-		if(strcmp(argv[1], "-a") == 0){
-			showFiles(true, path);	
-		}
-		else if(strcmp(argv[1], "-l") == 0)
-			fullOpt(path);
-		else if(strcmp(argv[1], "-R") == 0)
-			showRec(path);
-		else
-			showFiles(false, argv[1]);
-	} else {
-		showFiles(false, path);
-	}
-	cout << "\n";
-	return 0;
+    char pathname[128];
+    getcwd(pathname, 128);
+    if(argc == 1){
+        ls(pathname, 0);
+    } else {
+        if(strcmp(argv[1], "-l") == 0) {
+            ls_l(pathname);
+        } else if(strcmp(argv[1], "-r") == 0) {
+            ls_r(pathname);
+        } else {
+            ls(pathname, 1);  // ls -a
+        }
+    }
+    printf("\n");
+    return 0;
 }
